40c826219dc2aad9c8da8e755bc984a6c9dcae97
tokmac <lyubomir.mitkov@gmail.com>
2018-11-30 12:53:29 +0200

    Added UseCronus

--- src/Elders.Cronus.AspNetCore/Cronus.AspNetCore.rn.md
+++ src/Elders.Cronus.AspNetCore/Cronus.AspNetCore.rn.md
@@ -1,2 +1,5 @@
+#### 5.0.1 - 30.11.2015
+* Added UseCronus
+
 #### 5.0.0 - 29.11.2015
 * Initial version
---
+++ src/Elders.Cronus.AspNetCore/CronusHostExtension.cs
@@ -0,0 +1,43 @@
+﻿using Elders.Cronus.MessageProcessing;
+using Elders.Cronus.Multitenancy;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Elders.Cronus.AspNetCore
+{
+    public static class CronusExtension
+    {
+        public static IApplicationBuilder UseCronus(this IApplicationBuilder app)
+        {
+            var serviceProvider = app.ApplicationServices;
+            var cronusHost = serviceProvider.GetService<ICronusHost>();
+            var tenants = serviceProvider.GetService<ITenantList>();
+
+
+            foreach (var tenant in tenants.GetTenants())
+            {
+                using (var scope = serviceProvider.CreateScope())
+                {
+                    CronusContext context = scope.ServiceProvider.GetRequiredService<CronusContext>();
+                    context.Initialize(tenant, scope.ServiceProvider);
+                }
+            }
+
+
+            cronusHost.Start();
+
+            return app;
+        }
+
+        public static IApplicationBuilder StopCronus(this IApplicationBuilder app)
+        {
+            var serviceProvider = app.ApplicationServices;
+            var cronusHost = serviceProvider.GetService<ICronusHost>();
+
+            cronusHost.Stop();
+
+            return app;
+        }
+    }
+
+}
