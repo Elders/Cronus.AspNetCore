242a38cfcc19ddce7bdf3fc370b74c371198c14b
tokmac <lyubomir.mitkov@gmail.com>
2018-12-03 15:52:56 +0200

    Changed UseCronus to register a background hosted service responsible for CronusHost

--- src/Elders.Cronus.AspNetCore/Cronus.AspNetCore.rn.md
+++ src/Elders.Cronus.AspNetCore/Cronus.AspNetCore.rn.md
@@ -1,4 +1,7 @@
 #### 5.0.1 - 30.11.2015
+* Changed UseCronus to register a background hosted service responsible for CronusHost
+
+#### 5.0.1 - 30.11.2015
 * Added UseCronus
 
 #### 5.0.0 - 29.11.2015
--- src/Elders.Cronus.AspNetCore/CronusHostExtension.cs
+++ src/Elders.Cronus.AspNetCore/CronusHostExtension.cs
@@ -1,42 +1,72 @@
-﻿using Elders.Cronus.MessageProcessing;
+﻿using Elders.Cronus.AspNetCore.Logging;
+using Elders.Cronus.MessageProcessing;
 using Elders.Cronus.Multitenancy;
-using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Elders.Cronus.AspNetCore
 {
     public static class CronusExtension
     {
-        public static IApplicationBuilder UseCronus(this IApplicationBuilder app)
+        public static IServiceCollection UseCronus(this IServiceCollection services)
         {
-            var serviceProvider = app.ApplicationServices;
-            var cronusHost = serviceProvider.GetService<ICronusHost>();
-            var tenants = serviceProvider.GetService<ITenantList>();
+            services.AddHostedService<CronusHostService>();
 
+            return services;
+        }
+    }
+
+
+
+    public class CronusHostService : IHostedService, IDisposable
+    {
+        private static readonly ILog log = LogProvider.GetLogger(typeof(CronusHostService));
+
+        private readonly IConfiguration configuration;
+        private readonly ICronusHost cronusHost;
+
+        public CronusHostService(IConfiguration configuration, IServiceProvider provider, ITenantList tenants, ICronusHost cronusHost)
+        {
+            this.configuration = configuration;
+            this.cronusHost = cronusHost;
 
             foreach (var tenant in tenants.GetTenants())
             {
-                using (var scope = serviceProvider.CreateScope())
+                using (var scope = provider.CreateScope())
                 {
                     CronusContext context = scope.ServiceProvider.GetRequiredService<CronusContext>();
                     context.Initialize(tenant, scope.ServiceProvider);
                 }
             }
+        }
 
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            log.Info("Starting Cronus Host service...");
 
             cronusHost.Start();
 
-            return app;
+            log.Info("Service Cronus Host started");
+            return Task.CompletedTask;
         }
 
-        public static IApplicationBuilder StopCronus(this IApplicationBuilder app)
+        public Task StopAsync(CancellationToken cancellationToken)
         {
-            var serviceProvider = app.ApplicationServices;
-            var cronusHost = serviceProvider.GetService<ICronusHost>();
+            log.Info("Stopping Cronus Host service...");
 
             cronusHost.Stop();
 
-            return app;
+            log.Info("Service Cronus Host stopped");
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            StopAsync(CancellationToken.None).GetAwaiter().GetResult();
         }
     }
 
