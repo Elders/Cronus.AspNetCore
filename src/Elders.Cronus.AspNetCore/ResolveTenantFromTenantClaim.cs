219ccf0de86246311c3fa4e66853186ae7a5fa1d
Konstantin Tenekedzhiev <konstantin.tenekedzhiev@eldersoss.com>
2022-07-07 10:40:09 +0300

    fix: check the HttpContext before resolving the tenant

--- src/Elders.Cronus.AspNetCore/ResolveTenantFromTenantClaim.cs
+++ src/Elders.Cronus.AspNetCore/ResolveTenantFromTenantClaim.cs
@@ -1,9 +1,9 @@
-﻿using Elders.Cronus.AspNetCore.Exeptions;
+﻿using System;
+using System.Linq;
+using Elders.Cronus.AspNetCore.Exeptions;
 using Elders.Cronus.Multitenancy;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Options;
-using System;
-using System.Linq;
 
 namespace Elders.Cronus.AspNetCore
 {
@@ -29,6 +29,11 @@ namespace Elders.Cronus.AspNetCore
 
         private string ResolveTenant(HttpContext context)
         {
+            if (context is null) throw new ArgumentNullException(nameof(context));
+            if (context.User is null) throw new UnableToResolveTenantExeption($"Unable to resolve tenant. There is no ClaimsPrincipal. {nameof(HttpContext.User)}");
+            if (context.User.Claims.Any() == false)
+                throw new UnableToResolveTenantExeption("Unable to resolve tenant. Claims collection is empty. Did you configure the correct authority and audience?");
+
             var tenantClaim = context.User.Claims.Where(claim => claim.Type.Equals("tenant", StringComparison.OrdinalIgnoreCase) || claim.Type.Equals("tenant_client", StringComparison.OrdinalIgnoreCase) || claim.Type.Equals("client_tenant", StringComparison.OrdinalIgnoreCase)).FirstOrDefault();
             string tenant = tenantClaim?.Value;
 
